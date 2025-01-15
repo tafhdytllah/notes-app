ddf85f95604dda16910665dfa21b1774fd975a68
Taufik Hidayatullah <taufikhh.97@gmail.com>
2025-01-15 11:13:49 +0700

    fix host prod & dev

--- src/server.js
+++ src/server.js
@@ -11,8 +11,8 @@ const init = async () => {
 
   const server = Hapi.server({
     port: process.env.PORT,
-    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    host: process.env.HOST,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    // host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
